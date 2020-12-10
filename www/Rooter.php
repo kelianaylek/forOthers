9cbc39af1aba9c6a81f74aa2b66505d711365374
Kélian <kelian.raoult_severac@edu.devinci.fr>
2020-12-10 11:10:43 +0100

    Start project !

---
+++ www/Rooter.php
@@ -0,0 +1,15 @@
+<?php
+
+// use App\Controller\ConnexionController;
+
+if (array_key_exists("page", $_GET)) {
+
+    switch ($_GET["page"]) {
+        // case 'connexion':
+        //     $controller = new ConnexionController();
+        //     $controller->connexion();
+        //     break;
+    }
+} else {
+    // $controller = new ConnexionController();
+}
---
+++ www/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+define("ROOT", dirname(__DIR__));
+
+
+require ROOT."/Autoloader.php";
+session_start();
+Autoloader::register();
+
+require ROOT."/Rooter.php";
